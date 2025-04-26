fa24b1471996ee0fe73b7ffdb325b529527ce80a
smallwanderer <mcgratituve@gmail.com>
2025-04-26 15:38:46 +0900

    EDA 파이프라인 수정
    1. 파이프라인 통합
    2. yaml 기반 동적 EDA 기능 추가
    3; 원핫인코딩 드랍 기능 추가
    4. 자동 파일 생성 및 레포트 생성 추가
    5. 숫자 추출 오류 해결

--- eda.py
+++ eda.py
@@ -1,16 +1,125 @@
+from preprocessing import *
+import os
 import pandas as pd
+import yaml
+import shutil
 from ydata_profiling import ProfileReport
 
-dataset = './datasets/preprocessed01.csv'
+name = 'preprocessed02'
+dataset_dir = './datasets'
+
+print(f"Making File Name {name} ... ")
+if not os.path.isdir(f"{dataset_dir}/{name}"):
+  os.makedirs(f"{dataset_dir}/{name}")
+
+print(f"Copying Config File to {dataset_dir}/{name}")
+if not os.path.isfile(f"{dataset_dir}/{name}/{name}_config.yaml"):
+  shutil.copy("./preprocessing_config.yaml", f"{dataset_dir}/{name}")
+  os.rename(f'{dataset_dir}/{name}/preprocessing_config.yaml', f'{dataset_dir}/{name}/{name}_config.yaml')
+
+preprocessor_classes = {
+    "GroupMeanPreprocessor": GroupMeanPreprocessor,
+    "FillPreprocessor": FillPreprocessor,
+    "LabelEncoding": LabelEncoding,
+    "OneHotEncoding": OneHotEncoding,
+    "TargetEncodingKFold": TargetEncodingKFold,
+    "DictMapping": DictMapping,
+    "ExtractNumber" : ExtractNumber,
+}
+
+class PreprocessingPipeline:
+  def __init__(self, steps):
+    """
+    Parameters:
+        steps (list): (name, transformer)
+    """
+    self.steps = steps
+
+  def fit(self, df):
+    for name, transformer in self.steps:
+      print(f"Fitting: {name}")
+      transformer.fit(df)
+    return self
+
+  def transform(self, df):
+    for name, transformer in self.steps:
+      print(f"Transforming: {name}")
+      df = transformer.transform(df)
+    return df
+
+  def fit_transform(self, df):
+    self.fit(df)
+    return self.transform(df)
+
+class PreprocessingPipelineFromConfig:
+  def __init__(self, config_path):
+    self.config_path = config_path
+    self.steps = []
+
+  def load_config(self):
+    with open(self.config_path, 'r') as file:
+      config = yaml.safe_load(file)
+
+    for step_cfg in config['preprocessing']:
+      name = step_cfg['name']
+      cls_name = step_cfg['class']
+      params = step_cfg.get('params', {})
+
+      transformer_class = preprocessor_classes[cls_name]
+      transformer = transformer_class(**params)
+
+      self.steps.append((name, transformer))
+
+    return PreprocessingPipeline(self.steps)
+
+train = './datasets/train.csv'
+test = './datasets/test.csv'
+train_df = pd.read_csv(train)
+test_df = pd.read_csv(test)
+
+config_path = f'{dataset_dir}/{name}/{name}_config.yaml'
+builder = PreprocessingPipelineFromConfig(config_path)
+pipeline = builder.load_config()
+
+train_df = pipeline.fit(train_df).transform(train_df)
+train_df.to_csv(f"{dataset_dir}/{name}/preprocessed02.csv")
+
+pd.set_option('display.max_columns', None)
+print(train_df.head())
+
+profile = ProfileReport(train_df, title="Train Data EDA Report", explorative=True)
+profile.to_file(f"{dataset_dir}/{name}/{name}_eda_report.html")
+test_df = pipeline.transform(test_df)
+
+"""
+# 기능 테스트
+print("Train Fill Out 실행")
+# 'Guest_Popularity_percentage', 'Episode_Length_minutes'
+group_inputer = GroupMeanPreprocessor(target_col=['Guest_Popularity_percentage', 'Episode_Length_minutes'])
+group_inputer.fit(train_df)
+train_df = group_inputer.transform(train_df)
+
+# 'Number_of_Ads'
+fill_inputer = FillPreprocessor(method='median', target=['Number_of_Ads'])
+fill_inputer.fit(train_df)
+train_df = fill_inputer.transform(train_df)
+
+print("Train Encoding 실행")
+# 'Podcast_Name', 'Publication_Day', 'Publication_Time'
+label_encoder = LabelEncoding(nominal_col=['Podcast_Name', 'Publication_Day', 'Publication_Time'])
+label_encoder.fit(train_df)
+train_df = label_encoder.transform(train_df)
+
+# 'Genre'
+onehot_encoder = OneHotEncoding('Genre')
+onehot_encoder.fit(train_df)
+train_df = onehot_encoder.transform(train_df)
+
+# 'Episode_Sentiment'
+dict_encoder = DictMapping('Episode_Sentiment', {'Positive': 1, 'Neutral': 0, 'Negative': -1})
+train_df = dict_encoder.transform(train_df)
+
 pd.set_option('display.max_columns', None)  # 모든 열 출력
-pd.set_option('display.width', 1000)        # 한 줄 최대 길이 늘리기
-pd.set_option('display.max_rows', 100)      # 최대 행 수도 필요 시 조절
-df = pd.read_csv(dataset)
-
-print(df.shape)
-print(df.dtypes)
-print(df.isnull().sum())
-print(df.describe())
-
-profile = ProfileReport(df, title="Train Data EDA Report", explorative=True)
-profile.to_file("preprocessed01_eda_report.html")
+pd.set_option('display.max_rows', 10)  # 최대 행 수도 필요 시 조절
+print(train_df.head())
+"""
